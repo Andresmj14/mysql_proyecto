2a0a72017dd0e07a8887f31bec722d80efe8912e
Andresmj14 <andresmjs11@gmail.com>
2025-07-16 03:12:39 -0500

    feat: :art: inicio_consulta

--- consultas_especializadas/consultas.sql
+++ consultas_especializadas/consultas.sql
@@ -50,3 +50,65 @@ LIMIT 5;
 
 -- 3. Como gerente de ventas, quiero ver la distribución de productos por categoría y unidad de medida
 
+SELECT cat.description AS categoria, 
+um.description AS unidad_medida,
+COUNT(p.id) AS cantidad_productos,
+round(AVG(cp.price), 2) AS precio_promedio,
+MIN(cp.price) AS precio_minimo,
+MAX(cp.price) AS precio_maximo
+FROM products p 
+JOIN categories cat ON p.category_id = cat.id 
+JOIN companyproducts cp ON p.id = cp.product_id 
+JOIN unitofmeasure um ON cp.unitmeasure_id = um.id 
+GROUP BY cat.description, um.description
+ORDER BY cat.description, cantidad_productos DESC;
+
+-- 4.Como cliente, quiero saber qué productos tienen calificaciones superiores al promedio general.
+SELECT p.name AS 'Productos mejor calificados'
+FROM products p
+JOIN quality_products q ON p.id = q.product_id
+GROUP BY p.name
+HAVING AVG(q.rating) > (SELECT AVG(rating) FROM quality_products)
+ORDER BY AVG(q.rating) DESC LIMIT 6;
+
+--6. Como operador, deseo obtener los productos que han sido añadidos como favoritos por más de 10 clientes distintos.
+
+ SELECT p.name as productos, df.favorite_id as favorito, fv.customer_id as cliente
+ FROM products p 
+ JOIN details_favorites df ON p.id = df.product_id
+ JOIN favorites fv ON df.favorite_id = fv.id
+ ORDER BY df.favorite_id, p.name DESC LIMIT 10;
+
+SUBCONSULTAS
+
+--1. Como gerente, quiero ver los productos cuyo precio esté por encima del promedio de su categoría.
+
+SELECT 
+    p.name AS 'Producto Premium',
+    cat.description AS 'Categoría',
+    CONCAT('$', FORMAT(cp.price, 0)) AS 'Precio',
+    CONCAT('$', FORMAT(
+        (SELECT AVG(cp2.price) 
+         FROM companyproducts cp2 
+         JOIN products p2 ON cp2.product_id = p2.id 
+         WHERE p2.category_id = p.category_id), 0
+    )) AS 'Promedio Categoría'
+FROM 
+    products p
+JOIN 
+    companyproducts cp ON p.id = cp.product_id
+JOIN 
+    categories cat ON p.category_id = cat.id
+WHERE 
+    cp.price > (
+        SELECT AVG(cp2.price) 
+        FROM companyproducts cp2 
+        JOIN products p2 ON cp2.product_id = p2.id 
+        WHERE p2.category_id = p.category_id
+    )
+ORDER BY 
+    p.name;
+
+-- 2. Como administrador, deseo listar las empresas que tienen más productos que la media de empresas.
+
+
